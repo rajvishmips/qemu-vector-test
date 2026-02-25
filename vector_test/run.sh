243672c74424e7edcfedcaabd29c5d3439bb3368
Raj Vishwanathan <Raj.Vishwanathan@gmail.com>
2026-02-24 17:07:17 -0800

    Fix Basedir in run.sh

--- vector_test/run.sh
+++ vector_test/run.sh
@@ -1,4 +1,4 @@
-BASEDIR=/home/rvishwanathan/code/qemu-vector
+BASEDIR=..
 QEMU32=${BASEDIR}/qemu/build/qemu-riscv32
 #QEMU=${BASEDIR}/qemu/build/qemu-riscv64
 ${QEMU32} -cpu rv32,c=off,zce=on,zicond=true,v=true,vlen=1024   rvv
